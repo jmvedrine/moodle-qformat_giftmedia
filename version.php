334f6063cd9e0553ddc1e3a55143f69c3f7fdf1c
Jean-Michel Vedrine <vedrine@vedrine.org>
2013-04-11 17:52:56 +0200

    Changes for the MOODLE_22_STABLE version 1.0 release

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qformat_giftmedia';
-$plugin->version   = 2013041100;
-$plugin->requires  = 2012112900;
-$plugin->release   = '1.0';
+$plugin->version   = 2012121800;
+$plugin->requires  = 2011120507;
+$plugin->release   = '1.0 for Moodle 2.2.7';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'qformat_gift' => 2012112900
+    'qformat_gift' => 2011102700
 );
